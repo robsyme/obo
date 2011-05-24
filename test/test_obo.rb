e56ff59570779d72aed7a7c245089d941a41f6de
robsyme <rob.syme@gmail.com>
2011-05-24 17:56:54 +0800

    First draft

--- test/test_obo.rb
+++ test/test_obo.rb
@@ -1,7 +1,51 @@
 require 'helper'
+require "pp"
 
 class TestObo < MiniTest::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  def setup
+    @obo = Obo::Parser.new('test/data/ro.obo')
+#    @obo = Obo::Parser.new('test/data/so_2_4_3.obo')
   end
+
+  def test_header_parsing
+    header = @obo.elements.first
+    assert_equal Obo::Header, header.class
+    assert_equal '1.2', header['format-version']
+    assert_equal 'cjm', header['saved-by']
+    assert_equal 3, header['remark'].length
+  end
+
+  def test_stanza_parsing
+    first_stanza = @obo.elements.find{|element| element.is_a? Obo::Stanza}
+
+    assert_equal 'Typedef', first_stanza.name
+    assert_equal 'OBO_REL:is_a', first_stanza['id']
+    assert_equal 'is_a', first_stanza['name']
+    assert first_stanza['is_reflexive']
+  end
+
+  def test_fancy_methods
+    first_stanza = @obo.elements.find{|element| element.is_a? Obo::Stanza}
+    assert_equal 'OBO_REL:is_a', first_stanza.id
+    assert first_stanza.is_reflexive
+    assert first_stanza.is_reflexive?
+
+    assert_raises(NoMethodError){first_stanza.is_a_zebra?}
+  end
+
+  def test_readthrough
+    assert_equal 26, @obo.elements.count
+  end
+
+  def test_rewind
+    # Run through the file to the end
+    @obo.elements.count
+    assert_raises(StopIteration){@obo.elements.count}
+    @obo.rewind
+    assert_equal 26, @obo.elements.count
+  end
+  
 end
+
+
